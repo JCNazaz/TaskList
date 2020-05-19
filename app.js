18b6031da4ad8531115eaf60af75d49e2596068c
JCNazaz <araujo.s.fabiano@gmail.com>
2020-05-19 15:16:58 +0100

    Filter Working

--- app.js
+++ app.js
@@ -12,6 +12,12 @@ loadEventListeners();
 function loadEventListeners() {
   // Add Taks Event
   form.addEventListener("submit", addTask);
+  /* Remove Task */
+  taskList.addEventListener("click", removeTask);
+  /* Clear Tasks */
+  clearBtn.addEventListener("click", clearTasks);
+  /* Filter Tasks */
+  filter.addEventListener("keyup", filterTasks);
 }
 // Add Task
 
@@ -40,4 +46,30 @@ function addTask(e) {
   // Clear input
   taskInput.value = "";
   e.preventDefault();
+}
+function removeTask(e) {
+  if (e.target.parentElement.classList.contains("delete-item")) {
+    if (confirm("Are you sure?")) {
+      e.target.parentElement.parentElement.remove();
+    }
+  }
+}
+/* Clear Task */
+function clearTasks() {
+  while (taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild);
+  }
+}
+/* Filter Tasks */
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+
+  document.querySelectorAll(".collection-item").forEach(function (task) {
+    const item = task.firstChild.textContent;
+    if (item.toLowerCase().indexOf(text) != -1) {
+      task.style.display = "block";
+    } else {
+      task.style.display = "none";
+    }
+  });
 }
